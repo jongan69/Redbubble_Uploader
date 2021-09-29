379ec61324cc82cd4624d575def145b20b43a652
Jonathan Gan <jon@cryptocurrensea.io>
2021-09-28 21:26:51 -0400

    added git ignore

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Edit this file with your login info and the path to Chrome Driver
-email = "jonny2298@live.com"
-password = "Fuckthis123!"
+email = ""
+password = ""
 # Right click on Chrome driver -> Copy Path, paste below
-chromeDriverPath = '/Users/jonathan/Desktop/Folders/Redbubble_Automation/chromedriver'
+chromeDriverPath = ''
 # Add folder with your art to workspace -> Copy Path, paste below
-directory = '/Users/jonathan/Downloads'
+directory = ''
