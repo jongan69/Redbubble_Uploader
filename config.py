d6ec182944bac4bb31c8d59333b604051d3b5e9c
Jonathan Gan <jon@cryptocurrensea.io>
2021-10-01 13:11:04 -0400

    fixed code

---
+++ config.py
@@ -0,0 +1,6 @@
+# Edit this file with your login info and the path to Chrome Driver
+email = ""
+password = ""
+# Right click on Chrome driver -> Copy Path, paste below
+chromeDriverPath = 'Code/Redbubble_Automation/chromedriver'
+directory = 'Code/Redbubble_Automation/memes_folder'
